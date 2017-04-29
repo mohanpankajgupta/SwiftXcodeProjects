c8d1a32a52970feae1d35849afd17c027cace050
Pankaj Gupta <pankajgupta@Pankajs-MacBook-Pro.local>
2017-04-29 20:39:03 +0530

    added new autolayout project to SwiftXcodeProjects repo

---
+++ AutoLayout/AutoLayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoLayout
+//
+//  Created by Pankaj Gupta on 29/04/17.
+//  Copyright © 2017 Pankaj Gupta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
